4499540fe01f670465709d69599768e90a3f25e1
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2021-01-27 21:07:11 +0100

    data scope from fluxx
    data scope as defined in fluxx projection description

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -3,5 +3,42 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
 }});
 
 var siteConfig = {
-  rootPredicate: { type: 'equals', key: 'taxonKey', value: 5 }
+  rootPredicate: {
+    "type": "or",
+    "predicates": [
+      {
+        "type": "equals",
+        "key": "gadmGid",
+        "value": "USA.12_1"
+      },
+      {
+        "type":"in",
+        "key": "countryCode",
+        "values": [
+          "AS",
+          "CK",
+          "TL",
+          "FM",
+          "FJ",
+          "PF",
+          "GU",
+          "KI",
+          "MH",
+          "NR",
+          "NC",
+          "NU",
+          "MP",
+          "PW",
+          "PG",
+          "WS",
+          "SB",
+          "TK",
+          "TO",
+          "TV",
+          "VU",
+          "WF"
+        ]
+      }
+    ]
+  }
 };
