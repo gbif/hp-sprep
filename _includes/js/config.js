cbd7145ed03c458938ef14cecd4e956fb12150fb
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2021-01-28 15:44:10 +0100

    Update config.js
    Hi @aserawayneasera
    I've updated the scope again. Feel free to change it, this was just my best guess

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -4,40 +4,50 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
 
 var siteConfig = {
   rootPredicate: {
-    "type": "or",
+    "type": "and",
     "predicates": [
-      {
-        "type": "equals",
-        "key": "gadmGid",
-        "value": "USA.12_1"
+   		{
+        "type": "or",
+        "predicates": [
+          {
+            "type": "equals",
+            "key": "gadmGid",
+            "value": "USA.12_1"
+          },
+          {
+            "type":"in",
+            "key": "countryCode",
+            "values": [
+              "AS",
+              "CK",
+              "TL",
+              "FM",
+              "FJ",
+              "PF",
+              "GU",
+              "KI",
+              "MH",
+              "NR",
+              "NC",
+              "NU",
+              "MP",
+              "PW",
+              "PG",
+              "WS",
+              "SB",
+              "TK",
+              "TO",
+              "TV",
+              "VU",
+              "WF"
+            ]
+          }
+        ]
       },
       {
-        "type":"in",
-        "key": "countryCode",
-        "values": [
-          "AS",
-          "CK",
-          "TL",
-          "FM",
-          "FJ",
-          "PF",
-          "GU",
-          "KI",
-          "MH",
-          "NR",
-          "NC",
-          "NU",
-          "MP",
-          "PW",
-          "PG",
-          "WS",
-          "SB",
-          "TK",
-          "TO",
-          "TV",
-          "VU",
-          "WF"
-        ]
+        "type": "equals",
+        "key": "notIssues",
+        "value": "COUNTRY_COORDINATE_MISMATCH"
       }
     ]
   }
