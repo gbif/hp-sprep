f6b08787e34a98d68f69e9224240962a2ed36dee
aserawayneasera <27858318+aserawayneasera@users.noreply.github.com>
2021-04-26 16:05:24 +1300

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -1,4 +1,4 @@
-var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', extendWith: {
+var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'dark', extendWith: {
   primary: themeStyle.colors.primary
 }});
 
