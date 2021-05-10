7b2198b174cf76abba877bbfaa1e97ee14adbe71
aserawayneasera <27858318+aserawayneasera@users.noreply.github.com>
2021-05-11 08:56:12 +1300

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -3,7 +3,7 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'dark', exte
 }});
 
 var siteConfig = {
-  occurrenceSearchTabs: ['TABLE', 'GALLERY', 'MAP', 'DATASET'],
+  occurrenceSearchTabs: ['TABLE', 'GALLERY', 'MAP', 'METRICS', 'TAXONOMY', 'DATASET'],
   highlightedFilters: ['countryCode', 'occurrenceStatus', 'taxonKey', 'year', 'occurrenceIssue'],
   rootPredicate: {
     "type": "and",
